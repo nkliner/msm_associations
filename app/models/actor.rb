23cdb7932352127714b6abc6a44a8fe4ca15b5c4
Nick Kliner <nkliner@chicagobooth.edu>
2017-05-23 17:42:42 -0500

    Validation comments

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ApplicationRecord
   # - name: must be present; must be unique in combination with dob
-  validates :name, :presence => true, uniqueness => { :scope => :dob }
+  # validates :name, :presence => true, uniqueness => { :scope => :dob }
 
   # - dob: no rules
   # - bio: no rules
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,9 @@
 class Character < ApplicationRecord
   # - movie_id: must be present
-  validates :movie_id, :presence => true
+  # validates :movie_id, :presence => true
 
   # - actor_id: must be present
-  validates :actor_id, :presence => true
+  # validates :actor_id, :presence => true
 
   # - name: no rules
 
